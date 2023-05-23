9c4491fb1d9c13c1cc1c5774b3aea1016a268d2c
JOAQUIN PIVA <47870976@ort>
2023-05-23 10:40:52 -0300

    creando models

---
+++ Models/Escape.cs
@@ -0,0 +1,40 @@
+public static class Escape
+{
+    static string[] incognitasSalas = new string[4]; 
+    static int estadoJuego = 1;
+
+
+    private static void InicializarJuego()
+    {
+        incognitasSalas[0] = "";
+        incognitasSalas[1] = "";
+        incognitasSalas[2] = "";
+        incognitasSalas[3] = "";
+
+    }
+    public static int GetEstadoJuego()
+    {
+        return estadoJuego;
+    }
+    public static bool ResolverSala(int Sala, string Incognita)
+    {
+       
+        if(incognitasSalas == null)
+        {
+            InicializarJuego();
+        }
+
+        if(Sala == estadoJuego)
+        {
+            if(Incognita == incognitasSalas[Sala -1])
+            {
+                estadoJuego= estadoJuego + 1;
+                return true;
+            }
+        }   
+        
+        return false;
+        
+    }
+
+}
