257887ea5ef92b65800c20498dbf765652af08ae
AGOSTINA BOTTARINI <47866440@ort>
2023-05-23 10:09:36 -0300

    Creando proyecto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP05_Bottarini_Piva.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
